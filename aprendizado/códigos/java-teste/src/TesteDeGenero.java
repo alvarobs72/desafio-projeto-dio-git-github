34efa6c3c344d1027c3d49c7e51840f60bfd39bb
Álvaro <alvarorandao@gmail.com>
2025-02-19 10:58:48 -0300

    teste de genero aprimorado.

--- aprendizado/códigos/java-teste/src/TesteDeGenero.java
+++ aprendizado/códigos/java-teste/src/TesteDeGenero.java
@@ -2,14 +2,16 @@ public class TesteDeGenero {
 
 public static void main (String [] args) {
    // Altere somente o valor da variável "sexo" para testar o código
-    char genero = 'M';
+    char genero = ' ';
 
     if (genero == 'M') {
         System.out.println(genero + " " +"Masculino");
     } if (genero == 'F') {
         System.out.println(genero + " " +"Feminino");
-    } if (genero != 'M' & genero != 'F') {
-        System.out.println(genero + " " + "Sexo inválido");
+    } if (genero == ' ') {
+        System.out.println("Nenhum genero foi informado");
+    }else if (genero != 'M' & genero != 'F') {
+        System.out.println(genero + " " +"Outros");
     }
 
 }
