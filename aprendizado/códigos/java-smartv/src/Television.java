1597cd94078a106b72d39e7be7af7cc6bba65dc8
Álvaro <alvarorandao@gmail.com>
2025-02-28 18:49:34 -0300

    novo projeto television, variáveis e métodos criados.

--- aprendizado/códigos/java-smartv/src/Television.java
+++ aprendizado/códigos/java-smartv/src/Television.java
@@ -4,4 +4,26 @@ public class Television {
     int canal = 0;
     int canalAntecessor = canal - 1;
     int canalSucessor = canal + 1;
+
+        public void ligar() {
+        ligada = true;
+    }
+        public void desligar() {
+        ligada = false;
+    }
+        public void aumentarVolume() {
+        volume++;
+    }
+        public void diminuirVolume() {
+        volume--;
+    }
+        public void aumentarCanal() {
+        canal++;
+    }
+        public void diminuirCanal() {
+        canal--;
+    }
+        public void trocarCanal(int novoCanal) {
+        canal = novoCanal;
+    }
 }
---
+++ aprendizado/códigos/java-smartv/src/Usuario.java
@@ -0,0 +1,6 @@
+public class Usuario {
+
+    public static void main (String[] args) throws Exception {
+        Television tv = new Television();
+    }
+}
